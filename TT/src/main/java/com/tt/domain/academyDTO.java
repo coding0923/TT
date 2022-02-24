8aad92765b123fd011b747f63d7d5d54b600e123
DESKTOP-08J8C5F\ekgp4 <dh2806@naver.com>
2022-02-24 14:32:20 +0900

    학원, 과목, 커리큘럼, 티칭이력 dto 생성

---
+++ TT/src/main/java/com/tt/domain/academyDTO.java
@@ -0,0 +1,15 @@
+package com.tt.domain;
+
+import lombok.Data;
+
+@Data
+public class academyDTO {
+
+	private String academyId;
+
+	private String academyName;
+
+	private String academyLocation;
+
+	private String academyContact;
+}
---
+++ TT/src/main/java/com/tt/domain/subjectDTO.java
@@ -0,0 +1,23 @@
+package com.tt.domain;
+
+import lombok.Data;
+
+@Data
+public class subjectDTO {
+
+	private int subjectCode;
+
+	private String subjectName;
+
+	private String subjectNcsNumber;
+
+	private String subjectNcsLevel;
+
+	private String subjectInsertUser;
+
+	private String subjectUpdateUser;
+
+	private String subjectInsertDate;
+
+	private String subjectUpdateDate;
+}
