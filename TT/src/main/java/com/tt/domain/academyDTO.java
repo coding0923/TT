2caea1600afb7b719be61425166155eafdf3ceb3
leejy2296 <leejy12341266@gmail.com>
2022-02-24 15:11:31 +0900

    academyDTO 수정, commonDTO 생성

--- TT/src/main/java/com/tt/domain/academyDTO.java
+++ TT/src/main/java/com/tt/domain/academyDTO.java
@@ -5,11 +5,9 @@ import lombok.Data;
 @Data
 public class academyDTO {
 
-	private String academyId;
+    private String academyName;
 
-	private String academyName;
+    private String academyLocation;
 
-	private String academyLocation;
-
-	private String academyContact;
+    private String academyContact;
 }
