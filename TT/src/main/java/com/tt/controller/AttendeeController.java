f773254b2b010ba63820fd42bb6ccdde02274fa4
DESKTOP-08J8C5F\ekgp4 <dh2806@naver.com>
2022-03-07 16:20:58 +0900

    상담이력 저장

--- TT/src/main/java/com/tt/controller/AttendeeController.java
+++ TT/src/main/java/com/tt/controller/AttendeeController.java
@@ -20,101 +20,111 @@ import com.tt.service.AttendeeService;
 
 @Controller
 public class AttendeeController {
-    @Autowired
-    AttendeeService attendeeservice;
+	@Autowired
+	AttendeeService attendeeservice;
 
-    @GetMapping("/attendee/attendeelist")
-    public String GetAttendeeList(Model model, HttpSession session) {
-        Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
-        List<Map<String, String>> AttendeeList = attendeeservice.AttendeeList(academyclass);
-        System.out.println("리스트" + AttendeeList);
-        model.addAttribute("AttendeeList", AttendeeList);
-        return "attendee/attendeelist";
-    }
+	@GetMapping("/attendee/attendeelist")
+	public String GetAttendeeList(Model model, HttpSession session) {
+		Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
+		List<Map<String, String>> AttendeeList = attendeeservice.AttendeeList(academyclass);
+		System.out.println("리스트" + AttendeeList);
+		model.addAttribute("AttendeeList", AttendeeList);
+		return "attendee/attendeelist";
+	}
 
-    @PostMapping("/attendee/attendeeput")
-    @ResponseBody
-    public int PostAttendeeput(@RequestParam(value = "userIds[]", required = false) AttendeeDTO attendeedto,
-            HttpSession session) {
-        System.out.println("AttendeeputPost진입");
-        int result = 0;
-        attendeeservice.Attendeeput(attendeedto);
+	@PostMapping("/attendee/attendeeput")
+	@ResponseBody
+	public int PostAttendeeput(@RequestParam(value = "userIds[]", required = false) AttendeeDTO attendeedto,
+			HttpSession session) {
+		System.out.println("AttendeeputPost진입");
+		int result = 0;
+		attendeeservice.Attendeeput(attendeedto);
 
-        return result;
-    }
+		return result;
+	}
 
-    @PostMapping("/attendee/attendeecall")
-    @ResponseBody
-    public int PostAttendeeCall(@RequestParam(value = "userIds[]", required = false) List<String> checkArray,
-            HttpSession session) {
-        System.out.println("AttendeeCallPost진입");
-        int result = 0;
-        Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
-        TeacherDTO teacherdto = (TeacherDTO) session.getAttribute("loginUser");
-        for (String id : checkArray) {
-            AttendeeDTO attendeedto = new AttendeeDTO();
-            attendeedto.setUpdateUser(teacherdto.getTeacherId());
-            attendeedto.setParticipateStatus("수강중");
-            attendeedto.setAcademyId(academyclass.get("academyId"));
-            attendeedto.setClassId(academyclass.get("classId"));
-            attendeedto.setStudentId(id);
-            result = attendeeservice.AttendeeCall(attendeedto);
-            System.out.println("수락시 값" + attendeedto);
-        }
-        return result;
-    }
+	@PostMapping("/attendee/attendeecall")
+	@ResponseBody
+	public int PostAttendeeCall(@RequestParam(value = "userIds[]", required = false) List<String> checkArray,
+			HttpSession session) {
+		System.out.println("AttendeeCallPost진입");
+		int result = 0;
+		Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
+		TeacherDTO teacherdto = (TeacherDTO) session.getAttribute("loginUser");
+		for (String id : checkArray) {
+			AttendeeDTO attendeedto = new AttendeeDTO();
+			attendeedto.setUpdateUser(teacherdto.getTeacherId());
+			attendeedto.setParticipateStatus("수강중");
+			attendeedto.setAcademyId(academyclass.get("academyId"));
+			attendeedto.setClassId(academyclass.get("classId"));
+			attendeedto.setStudentId(id);
+			result = attendeeservice.AttendeeCall(attendeedto);
+			System.out.println("수락시 값" + attendeedto);
+		}
+		return result;
+	}
 
-    @PostMapping("/attendee/attendeegiveup")
-    @ResponseBody
-    public int PostAttendeeGiveUP(@RequestParam(value = "userIds[]", required = false) List<String> checkArray,
-            HttpSession session) {
-        System.out.println("AttendeeGiveUPPost진입");
-        int result = 0;
-        Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
-        TeacherDTO teacherdto = (TeacherDTO) session.getAttribute("loginUser");
-        for (String id : checkArray) {
-            AttendeeDTO attendeedto = new AttendeeDTO();
-            attendeedto.setUpdateUser(teacherdto.getTeacherId());
-            attendeedto.setParticipateStatus("중도포기");
-            attendeedto.setAcademyId(academyclass.get("academyId"));
-            attendeedto.setClassId(academyclass.get("classId"));
-            attendeedto.setStudentId(id);
-            result = attendeeservice.AttendeeGiveup(attendeedto);
-        }
-        return result;
-    }
+	@PostMapping("/attendee/attendeegiveup")
+	@ResponseBody
+	public int PostAttendeeGiveUP(@RequestParam(value = "userIds[]", required = false) List<String> checkArray,
+			HttpSession session) {
+		System.out.println("AttendeeGiveUPPost진입");
+		int result = 0;
+		Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
+		TeacherDTO teacherdto = (TeacherDTO) session.getAttribute("loginUser");
+		for (String id : checkArray) {
+			AttendeeDTO attendeedto = new AttendeeDTO();
+			attendeedto.setUpdateUser(teacherdto.getTeacherId());
+			attendeedto.setParticipateStatus("중도포기");
+			attendeedto.setAcademyId(academyclass.get("academyId"));
+			attendeedto.setClassId(academyclass.get("classId"));
+			attendeedto.setStudentId(id);
+			result = attendeeservice.AttendeeGiveup(attendeedto);
+		}
+		return result;
+	}
 
-    @PostMapping("/attendee/attendeedelete")
-    @ResponseBody
-    public int PostAttendeeDelete(@RequestParam(value = "userIds[]", required = false) List<String> checkArray,
-            HttpSession session) {
-        System.out.println("AttendeeDeletePost진입");
-        int result = 0;
-        Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
-        for (String id : checkArray) {
-            AttendeeDTO attendeedto = new AttendeeDTO();
-            attendeedto.setAcademyId(academyclass.get("academyId"));
-            attendeedto.setClassId(academyclass.get("classId"));
-            attendeedto.setStudentId(id);
-            result = attendeeservice.AttendeeDelete(attendeedto);
-        }
-        return result;
-    }
+	@PostMapping("/attendee/attendeedelete")
+	@ResponseBody
+	public int PostAttendeeDelete(@RequestParam(value = "userIds[]", required = false) List<String> checkArray,
+			HttpSession session) {
+		System.out.println("AttendeeDeletePost진입");
+		int result = 0;
+		Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
+		for (String id : checkArray) {
+			AttendeeDTO attendeedto = new AttendeeDTO();
+			attendeedto.setAcademyId(academyclass.get("academyId"));
+			attendeedto.setClassId(academyclass.get("classId"));
+			attendeedto.setStudentId(id);
+			result = attendeeservice.AttendeeDelete(attendeedto);
+		}
+		return result;
+	}
 
-    @GetMapping("/attendee/attendeedetaillist")
-    public String GetAttendeeDetailList(@RequestParam(value = "studentId", required = false) String studentId,
-            Model model, HttpSession session) {
-        Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
-        AttendeeDTO attendeedto = new AttendeeDTO();
-        attendeedto.setStudentId(studentId);
-        attendeedto.setAcademyId(academyclass.get("academyId"));
-        attendeedto.setClassId(academyclass.get("classId"));
-        Map<String, String> attendeeDetail = attendeeservice.AttendeeDetail(attendeedto);
-        List<CounsellingDTO> counsellingIdList = attendeeservice.CounsellingList(studentId);
-        System.out.println("리스트" + attendeeDetail);
-        System.out.println("CounsellingDTO" + counsellingIdList);
-        model.addAttribute("attendeeDetail", attendeeDetail);
-        model.addAttribute("counsellingIdList", counsellingIdList);
-        return "attendee/attendeedetaillist";
-    }
+	@GetMapping("/attendee/attendeedetaillist")
+	public String GetAttendeeDetailList(@RequestParam(value = "studentId", required = false) String studentId,
+			Model model, HttpSession session) {
+		Map<String, String> academyclass = (Map) session.getAttribute("academyclasss");
+		AttendeeDTO attendeedto = new AttendeeDTO();
+		attendeedto.setStudentId(studentId);
+		attendeedto.setAcademyId(academyclass.get("academyId"));
+		attendeedto.setClassId(academyclass.get("classId"));
+		Map<String, String> attendeeDetail = attendeeservice.AttendeeDetail(attendeedto);
+		List<CounsellingDTO> counsellingIdList = attendeeservice.CounsellingList(studentId);
+		System.out.println("리스트" + attendeeDetail);
+		System.out.println("CounsellingDTO" + counsellingIdList);
+		model.addAttribute("attendeeDetail", attendeeDetail);
+		model.addAttribute("counsellingIdList", counsellingIdList);
+		return "attendee/attendeedetaillist";
+	}
+
+	@PostMapping("/attendee/attendeedetaillist")
+	public String PostCounsellingInsert(CounsellingDTO dto) {
+		int result = attendeeservice.CounsellingInsert(dto);
+		if (result == 1) {
+			return "redirect:/attendee/attendeelist";
+		} else {
+			return "redirect:/attendee/attendeedetaillist";
+		}
+	}
 }
