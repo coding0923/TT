36ad4b925691b02c3c5346b5b025668d4cd0682d
kimsia0717 <https://Kimsia0717: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@github.com/Kimsia0717/teamC_campYo>
2022-03-10 13:56:14 +0900

    사이드바 설정

--- TT/src/main/resources/static/js/attendeedetail.js
+++ TT/src/main/resources/static/js/attendeedetail.js
@@ -1,21 +1,4 @@
 /**
  * 
  */
- $("#counsellingBtn").on("click",function(e){
-    e.preventDefault(); 
-    var counsellingdata ={
-           studentId : 'id', 
-           classId : 'classid',
-           academyId : 'academyId',
-           teacherId :'teacherId',
-           insertUser : 'insertUser',
-           updateUser : 'updateUser'
-    }
-    counsellingdata.studentId
-    counsellingdata.classId
-    counsellingdata.academyId
-    counsellingdata.teacherId
-    counsellingdata.insertUser
-    counsellingdata.updateUser
-    
-});
+ 
